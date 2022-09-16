22323c0eace98fccacd0540be70740358243592e
Mofe <moflux620@gmail.com>
2022-09-16 11:50:51 +0100

    ..updated login page

--- login.php
+++ login.php
@@ -328,11 +328,7 @@ return true;
 	</div><!--/.wrapper-->
 
 	<div class="footer">
-		<div class="container">
-			 
-
-			<b class="copyright">&copy; 2014 Edmin - EGrappler.com </b> All rights reserved.
-		</div>
+		
 	</div>
 	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
 	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
